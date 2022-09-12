dfedb5f3299a0c2ff9b027b7e8907a425161296a
dogmash69 <103027893+dogmash69@users.noreply.github.com>
2022-09-12 16:34:11 +0200

    maj 077

--- app.js
+++ app.js
@@ -17,17 +17,22 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 const getUser = require("./getUsers");
 const { validateMovie, validateUser} = require("./validators");
+const { hashPassword } = require("./auth.js");
+
+
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", getUser.getUser);
 app.get("/api/users/:id", getUser.getUserById);
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", validateUser, getUser.postUser);
+app.post("/api/users", validateUser, hashPassword, getUser.postUser);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 app.put("/api/users/:id", validateUser, getUser.updateUser);
 app.delete("/api/movies/:id", movieHandlers.delMovie);
 app.delete("/api/users/:id", getUser.delUser);
+
 
 
 
--- getUsers.js
+++ getUsers.js
@@ -1,7 +1,7 @@
 const database = require("./database");
 
 const getUser = (req, res) => {
-  const initialSql = "select * from users";
+  const initialSql = "select id, firstname, lastname, email, city, language from users;";
   const where = [];
 
   if (req.query.language != null) {
@@ -56,12 +56,12 @@ const getUserById = (req, res) => {
 };
 
 const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
   database
     .query(
-      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-      [firstname, lastname, email, city, language]
+      "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language, hashedPassword]
     )
     .then(([result]) => {
       res.location(`/api/users/${result.insertId}`).sendStatus(201);
